4a0d4cf144d0b7350c70e3bdccd26ce70ae5a18d
Ladislav Foldyna <foldyna@gmail.com>
2023-04-11 09:29:11 +0200

    Added A1Club, AGCWDL, CWOPS, eQSLAG, FISTS, FOC, LIDS, LOTW, NAQCC, SKCC

---
+++ updaters/lib/generic.sh
@@ -0,0 +1,30 @@
+LISTS_DIR=../../lists
+CONTENT_FILE=../../lists/content.csv
+
+function memberListHeader()
+{
+    echo "# $1"
+    echo "callsign,member_id,valid_from,valid_to"
+}
+
+function downloadFile()
+{ 
+    curl -A "user-agent-name QLog" -s "$1" --output ${LISTS_DIR}/${2}
+    echo $? 
+}
+
+function writeContentData()
+{
+    shortDesc="$1"
+    longDesc="$2"
+    filename="$3"
+    lastUpdate="$4"
+    numberRecords=$(("$5"-2))
+
+    #insert or update
+    [ -f ${CONTENT_FILE} ] && grep -q "^${shortDesc},${longDesc}" ${CONTENT_FILE} && sed -i "s/^${shortDesc},${longDesc}.*/${shortDesc},${longDesc},${filename},${lastUpdate},${numberRecords}/" ${CONTENT_FILE} || echo "${shortDesc},${longDesc},${filename},${lastUpdate},${numberRecords}" >> ${CONTENT_FILE}
+    tmpfile=$(mktemp)
+    sort ${CONTENT_FILE} > ${tmpfile}
+    mv ${tmpfile} ${CONTENT_FILE}
+}
+
